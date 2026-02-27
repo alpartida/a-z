6cc8d7006b423f35f002d096b11733bb91c4f5e7
Zach Queen <zaq1r276@gmail.com>
2026-02-26 16:56:13 -0800

    remove scroll speed

--- js/main.js
+++ js/main.js
@@ -217,7 +217,6 @@
 	var pxShow = 300; // height on which the button will show
 	var fadeInTime = 400; // how slow/fast you want the button to show
 	var fadeOutTime = 400; // how slow/fast you want the button to hide
-	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
 
 	// Show or hide the sticky footer button
 	jQuery(window).scroll(function () {
